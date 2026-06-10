62adb4f7a6673cb85d484dce42ca1fd65e41280a
soeun <lselse8112@gmacil.com>
2026-06-10 16:22:40 +0900

    BookService 도서 정보 수정 부분에 커버 이미지 수정 추가

--- backend/src/main/java/com/aivle/bookapp/service/BookService.java
+++ backend/src/main/java/com/aivle/bookapp/service/BookService.java
@@ -46,6 +46,7 @@ public class BookService {
         if (patchBook.getAuthor() != null) existingBook.setAuthor(patchBook.getAuthor());
         if (patchBook.getContent() != null) existingBook.setContent(patchBook.getContent());
         if (patchBook.getCategory() != null) existingBook.setCategory(patchBook.getCategory());
+        if (patchBook.getCoverImageUrl() != null) existingBook.setCoverImageUrl(patchBook.getCoverImageUrl());
 
         return bookRepository.save(existingBook);
     }
